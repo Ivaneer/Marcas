3fa688a103298856cdc60471d9e5d8d2a835bc9b
Ivan Hernandez <92162428+Ivaneer@users.noreply.github.com>
2022-02-09 13:59:55 +0100

    Add files via upload

--- Javascript/Profe/js/funciones3.js
+++ Javascript/Profe/js/funciones3.js
@@ -3,9 +3,11 @@ function cambiarEstado()
 	let nombre_clase=document.getElementById("start").className;
 	if (nombre_clase=="mostrado")
 		{document.getElementById("start").className="oculto";
+		document.getElementById("start").innerHTML="VERDE"
 	}
 	else
 		{document.getElementById("start").className="mostrado";
+		document.getElementById("start").innerHTML="ROJO"
 	}
 	
 }
